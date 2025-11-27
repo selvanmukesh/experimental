85df732da3b060e97f6966f3786b96b76138c2b3
mukesh <mukeshselvan0861@gmail.com>
2025-11-27 10:55:13 +0530

    c

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -33,8 +33,8 @@ export async function POST(req: NextRequest) {
     console.log("📩 Webhook Event Received:", JSON.stringify(body, null, 2));
 
 
-    if(body?.entry[0]?.messages){
-      console.log("webhook message----->",body.entry[0].messages);
+    if(body?.entry[0]?.changes[0]?.messages){
+      console.log("webhook message----->",body.entry[0].changes[0].messages);
 
     }else{
 
